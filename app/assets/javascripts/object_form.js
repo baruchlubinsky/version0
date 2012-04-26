1dc29d76deb77615b901d2a1486906b75b90d899
Baruch Lubinsky <baruchlubinsky@gmail.com>
2012-04-26 16:20:08 +0200

    javascript for objects

---
+++ app/assets/javascripts/object_form.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('#matrix_object_object_type').change(changeOptions);//.trigger('change');
+	changeOptions();
+});
+
+function changeOptions ()
+{
+	$('#attributes_div').html($('#' + $('#matrix_object_object_type option:selected').first().text().toLowerCase() + '_attributes').html());
+}
